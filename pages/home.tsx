75a76b0b4c7c3243dc1d5f2abdb9897ec83aa6d9
Tanush Saha <tanush.saha05@gmail.com>
2026-09-06 01:56:40 +0530

    chore: update app builder (#4)
    * chore: update app builder
    
    * chore: update app builder
    
    ---------
    
    Co-authored-by: FlowOS App Builder <flowos@app-builder.local>

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { flow } from '@/lib/flow'
-import { FlowTable } from '@/components/flowos' // Tanush Saha -  Targetting ServiceNow Developer
+import { FlowTable } from '@/components/flowos' // Tanush Saha -  Switching to Servicenow
 
 export default function HomePage() {
   const [showCreate, setShowCreate] = useState(false)
